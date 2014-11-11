f8fec303e2140214f13329e912699e08057cc40c
Michael Crumm <mike.crumm@groundctrl.com>
2014-11-11 13:02:37 -0800

    Adding bundles for functional testing, a console tool, and split bootstrap into separate files for functional/unit tests

--- src/Tests/bootstrap.php
+++ src/Tests/bootstrap.php
@@ -1,19 +1,5 @@
 <?php
 
-use Doctrine\Common\Annotations\AnnotationRegistry;
+$loader = require __DIR__.'/Functional/app/bootstrap.php';
 
-
-#@TODO: FIX PATHS *will be fixed when it's its own bundle
-#@todo: development env only tests
-$loader = require __DIR__.'/../../vendor/autoload.php';
-
-$loader->add('Ctrl\Bundle\ConcertoBundle\Tests\Fixtures', __DIR__ . '/Fixtures/');
-
-// intl
-
-if (!function_exists('intl_get_error_code')) {
-    require_once __DIR__.'/../../vendor/symfony/symfony/src/Symfony/Component/Intl/Resources/stubs/functions.php';
-}
-
-
-AnnotationRegistry::registerLoader([ $loader, 'loadClass' ]);
+// Add PHPUnit-specific bootstrapping here.
